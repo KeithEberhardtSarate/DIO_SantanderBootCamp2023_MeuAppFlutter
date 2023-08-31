a972bd5e69cc1e4fc713d0f067470adcc484e8c7
keith.sarate <keith.sarate@motorolasolutions.com>
2023-08-31 16:42:32 -0300

    Add create project

---
+++ android/app/src/main/kotlin/com/keith/sarate/firstflutterapp/dio_santander_bootcamp_2023_meuappflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.keith.sarate.firstflutterapp.dio_santander_bootcamp_2023_meuappflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
